ab8f871b2a4724483c50702099183c9087085e94
carlacarolinecs <carlacarolinecs1995@outlook.com>
2021-01-23 11:05:35 -0200

    Add project to git...

---
+++ Script/bug.R
@@ -0,0 +1,7 @@
+print("Adicionando um projeto já criado ao git")
+#Aperte na pasta diretorio de seu projeto, com o botão direito do mouse e logo após
+#clique na opção  git bach here. Ao abrir o terminal digite
+#pwd (aperte em enter)
+#git init (aperte em enter)
+#git add . (aperte em enter)
+#git commit -m "Initial commit" (aperte em enter)
